01cddf91d45188e8d7738f155484aa7bf6f30bda
Joe <48526917+jblsp@users.noreply.github.com>
2024-02-19 18:00:50 -0500

    changed gui handling

--- src/BaseFrame.java
+++ src/BaseFrame.java
@@ -1,17 +1,13 @@
 import java.awt.Font;
 import java.io.IOException;
 import javax.swing.*;
-import com.formdev.flatlaf.FlatDarkLaf;
 
-public class GUI {
+public class BaseFrame extends JFrame {
 
-    public GUI() {
-        FlatDarkLaf.setup();
-        JFrame frame = new JFrame();
-        frame.setSize(500, 120);
-        frame.setTitle("RKG Manager");
+    public BaseFrame() {
+        this.setTitle("RKG Manager");
         try {
-            RKG rkg = new RKG("02m08s6962311 EG2K.rkg");
+            RKG rkg = new RKG("02m20s470_092Kg0d.rkg");
             String time = String.format("%02d:%02d.%03d", rkg.getMinutes(), rkg.getSeconds(), rkg.getMilliseconds());
             String name = rkg.getMiiName();
             String lap1 = String.format("%02d:%02d.%03d", rkg.getLapMinutes(1), rkg.getLapSeconds(1),
@@ -24,12 +20,11 @@ public class GUI {
             JLabel ghost_label = new JLabel(ghost_info);
             ghost_label.setHorizontalAlignment(JLabel.CENTER);
             ghost_label.setVerticalAlignment(JLabel.CENTER);
-            ghost_label.setFont(new Font("Bahnschrift", Font.BOLD, 18));
-            frame.add(ghost_label);
+            ghost_label.setFont(new Font("Arial", Font.BOLD, 18));
+            this.add(ghost_label);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        frame.setVisible(true);
     }
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,41 @@
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.util.SystemInfo;
+
 public class Driver {
     public static void main(String[] args) {
-        new GUI();
+
+        if (SystemInfo.isMacOS) {
+            // enable screen menu bar
+            // (moves menu bar from JFrame window to top of screen)
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+
+            // application name used in screen menu bar
+            // (in first menu after the "apple" menu)
+            System.setProperty("apple.awt.application.name", "RKG Manager");
+
+            // appearance of window title bars
+            // possible values:
+            // - "system": use current macOS appearance (light or dark)
+            // - "NSAppearanceNameAqua": use light appearance
+            // - "NSAppearanceNameDarkAqua": use dark appearance
+            // (must be set on main thread and before AWT/Swing is initialized;
+            // setting it on AWT thread does not work)
+            System.setProperty("apple.awt.application.appearance", "system");
+        }
+
+        // Linux
+        if (SystemInfo.isLinux) {
+            // enable custom window decorations
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            JDialog.setDefaultLookAndFeelDecorated(true);
+        }
+
+        FlatDarkLaf.setup();
+
+        BaseFrame base_frame = new BaseFrame();
+        base_frame.pack();
+        base_frame.setVisible(true);
     }
 }
