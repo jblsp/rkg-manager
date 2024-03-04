7666274377788cf97d62882b4e5607a9fb013110
Joe <48526917+jblsp@users.noreply.github.com>
2024-03-03 22:13:37 -0500

    refactor main

--- src/main/java/rkgmanager/Main.java
+++ src/main/java/rkgmanager/Main.java
@@ -9,27 +9,17 @@ public class Main {
     public static void main(String[] args) {
 
         if (SystemInfo.isMacOS) {
-            // enable screen menu bar
-            // (moves menu bar from JFrame window to top of screen)
+            // enable screen menu bar (moves menu bar from JFrame to top of screen)
             System.setProperty("apple.laf.useScreenMenuBar", "true");
 
             // application name used in screen menu bar
-            // (in first menu after the "apple" menu)
             System.setProperty("apple.awt.application.name", "RKG Manager");
 
-            // appearance of window title bars
-            // possible values:
-            // - "system": use current macOS appearance (light or dark)
-            // - "NSAppearanceNameAqua": use light appearance
-            // - "NSAppearanceNameDarkAqua": use dark appearance
-            // (must be set on main thread and before AWT/Swing is initialized;
-            // setting it on AWT thread does not work)
-            System.setProperty("apple.awt.application.appearance", "system");
+            // sets dark title bar
+            System.setProperty("apple.awt.application.appearance", "NSAppearanceNameDarkAqua");
         }
 
-        // Linux
         if (SystemInfo.isLinux) {
-            // enable custom window decorations
             JFrame.setDefaultLookAndFeelDecorated(true);
             JDialog.setDefaultLookAndFeelDecorated(true);
         }
